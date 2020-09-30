099dd99d05565cbd52a984418de987821e37ef0a
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-09-30 20:49:10 +0000

    Procedimiento suma

--- 5. Recursión/listas.rkt
+++ 5. Recursión/listas.rkt
@@ -6,6 +6,7 @@
         (cons (car lst)
               (cons-fin x (cdr lst))))))
 
+;;; O(N)
 (define dup
   (lambda (lst)
     (if (null? lst)
@@ -13,3 +14,9 @@
         (cons (car lst)
               (cons (car lst)
                     (dup (cdr lst)))))))
+
+(define suma
+  (lambda (lst)
+    (if (null? lst)
+        0
+        (+ (car lst) (suma (cdr lst))))))
