3b9f7a608bcf00ffac1c67eae78f6a91c6788dae
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-10-02 21:25:54 +0000

    Factorial y torres de hanoi en C++

---
+++ 5. Recursión/recursion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * fact(n - 1);
+    }
+}
+
+void hanoi(int n, char origen, char destino, char temp)
+{
+    if (n > 0) {
+        hanoi(n - 1, origen, temp, destino);
+        cout << "Mover disco " << n << " de " << origen << " a "
+            << destino << endl;
+        hanoi(n - 1, temp, destino, origen);
+    }
+}
+
+int main()
+{
+    /*for (int i = 0; i < 20; ++i) {
+        cout << i << "! = " << fact(i) << endl;
+    }*/
+    hanoi(3, 'A', 'C', 'B');
+    return 0;
+}
